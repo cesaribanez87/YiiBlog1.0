8209af0d277b5f2ca0e982dab459140909049207
cesaralejandro <cesaribanez87@hotmail.com>
2015-06-26 19:10:23 -0500

    minor changes , show only first 5 post

--- protected/views/post/related_posts.php
+++ protected/views/post/related_posts.php
@@ -5,12 +5,16 @@
  * Date: 25/05/15
  * Time: 12:11 PM
  */
+$i=0;
 ?>
 <div>
     <dl>
         <?php foreach($post as $p): ?>
-            <dt><?php echo (strlen($p->post_title)>30)?substr($p->post_title,0,30).'...':$p->post_title; ?></dt>
-            <dd><?php echo (strlen($p->post_content)>180)?substr($p->post_content,0,180).'...':$p->post_content; ?></dd>
+            <?php if($i<5): ?>
+                <dt><?php echo (strlen($p->post_title)>30)?substr($p->post_title,0,30).'...':$p->post_title; ?></dt>
+                <dd><?php echo (strlen($p->post_content)>180)?substr($p->post_content,0,180).'...':$p->post_content; ?></dd>
+                <?php $i++; ?>
+            <?php endif; ?>
         <?php endforeach; ?>
     </dl>
 </div>
