c715896217a8d2af6bb4fad4676def0cb0bf3874
cesaralejandro <cesaribanez87@hotmail.com>
2015-06-27 03:49:56 -0500

    fix bug, validate category to prevent error.

--- protected/views/post/single.php
+++ protected/views/post/single.php
@@ -6,7 +6,7 @@
  * Time: 04:43 PM
  */
 ?>
-<?php date_default_timezone_set('UTC'); ?>
+<?php date_default_timezone_set('America/Mexico_City'); ?>
 <div>
     <h3><?php echo $post->post_title; ?></h3>
     <?php if(Yii::app()->user->id=='CesarIbanez'): ?>
@@ -28,7 +28,7 @@
 <div style="border-width: 5px; border-style: double; border-color: black;" align="right">
     <label>
         <?php echo 'Last Update: '.date('Y/m/d h:m:s',$post->modify_date); ?>,
-        <?php echo 'Category: '.$post->category->category; ?>,
+        <?php echo 'Category: '.!empty($post->category->category)?$post->category->category:'N/A'; ?>,
         Tags: Not Implemented Yet,
     </label>
 </div>
